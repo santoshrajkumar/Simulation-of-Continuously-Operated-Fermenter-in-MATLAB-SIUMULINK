403f1e7e6486ea0cdf4ef2f31f21763756b4c352
Santosh Mohan Rajkumar <rajkumar.nits@gmail.com>
2020-03-06 08:10:38 +0530

    Add files via upload

---
+++ bio_modelling_parameters.m
@@ -0,0 +1,33 @@
+clear all
+clc
+
+Yxs = 0.4;
+alpha = 2.2;
+beta = 0.2;
+mium = 0.48;
+Pm = 50;
+Km = 1.2;
+Ki = 22;
+%D = 0.15 + 0.65*0.15; %+0.3*0.15;
+ %Sf = 20 ;
+% X0 = 7.038;
+% S0 = 2.404;
+% P0 = 24.87;
+% %xo = X;
+% % S = S0;
+% % P = P0;
+% X= X0;
+% miu = (mium*(1-(P0/Pm))*S0)/(Km + S0 + (S0^(2)/Ki))
+% 
+% tspan = [0  20];
+% ode1 = @(t,x) eqone(D, pnew, X);
+% %[t,x] = ode23(ode1, tspan, xo);
+% 
+% ode3 = @(t,x) eqthree(D, P,alpha, pnew,beta, X);
+% [t,x] = ode45 (ode3, tspan, X);
+% 
+% plot(t,x)
+
+
+
+
